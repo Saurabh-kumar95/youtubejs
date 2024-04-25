21213c362a661e29663016e6f49f7e309ece49c4
Saurabh-kumar95 <154539057+Saurabh-kumar95@users.noreply.github.com>
2024-04-25 15:49:16 +0000

    data

---
+++ 01_basic/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict" //Treat all JS code as newer version
+
+// alert("Hello "); // We are using nodejs , not browser
+
+let name="Saurabh kumar";
+let age =18;
+let isLoggedIn=false;
+
+// number => 2 to power 53
+// Bignit
+// String => ""
+// Boolean => True/false
+// Null => Standalone value
+// undefined =>
+// Symbol => unique
+
+// object
+
+console.log(typeof name);
+
