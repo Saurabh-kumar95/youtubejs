77be1f9200f50122b35235880b15d7ff29c58ee6
Saurabh-kumar95 <154539057+Saurabh-kumar95@users.noreply.github.com>
2024-04-25 15:19:34 +0000

    datatype

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,8 @@
 console.log('saurabh')
+let city="Sitamarhi";
+console.log(city);
+const accontId="12334";
+let accountEmail="Saurabh95082@gamil.com";
+console.table([accontId,accountEmail,city,]);
+
+// Prefer not to use var because of issue scope and functionl scope 
