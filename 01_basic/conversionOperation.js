140f0a89eca14b6653bffe8bd3b48ef2756efa6a
Saurabh-kumar95 <154539057+Saurabh-kumar95@users.noreply.github.com>
2024-04-25 17:42:35 +0000

    conversionoperation

---
+++ 01_basic/conversionOperation.js
@@ -0,0 +1,11 @@
+let score="43hbs";
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);    
+console.log(valueInNumber);                     
+
+let somenum=23;
+let Stringnumber=String(somenum);
+console.log(Stringnumber);
+console.log(typeof Stringnumber);
