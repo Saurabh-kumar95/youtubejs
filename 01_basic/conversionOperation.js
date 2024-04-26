b0aab9ddabbb6522c6dc309d68d23f8401a8793c
Saurabh-kumar95 <154539057+Saurabh-kumar95@users.noreply.github.com>
2024-04-26 06:11:03 +0000

    operations

--- 01_basic/conversionOperation.js
+++ 01_basic/conversionOperation.js
@@ -1,11 +1,36 @@
 let score="43hbs";
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber);    
-console.log(valueInNumber);                     
+// console.log(typeof valueInNumber);    
+// console.log(valueInNumber);                     
 
 let somenum=23;
 let Stringnumber=String(somenum);
-console.log(Stringnumber);
-console.log(typeof Stringnumber);
+// console.log(Stringnumber);
+// console.log(typeof Stringnumber);
+
+// ********************** Operations *********************************
+
+let value=3;
+let negValue = +value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+let str1="Hello ";
+let str2="Saurabh ";
+let str3=str1+str2;
+// console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+
+
+console.log("1"+2+2);
+console.log(1+2+"2");
