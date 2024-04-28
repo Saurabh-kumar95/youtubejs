34b26650708ecfdaff5e5d29450c886b2c31b718
Saurabh-kumar95 <154539057+Saurabh-kumar95@users.noreply.github.com>
2024-04-28 06:26:05 +0000

    memorylocation heap and stack

--- 01_basic/datatypes-sumary.js
+++ 01_basic/datatypes-sumary.js
@@ -1,14 +1,14 @@
 //  Primitive 
 //  7 types: String ,Number ,Boolean ,Null ,undefined, Symbol,BigInt 
 
-const srt=233;
-const num=3432.9;
-const bool=false;
-const temp=null;
-const id=Symbol('2354');
-const anotherid=summbol('2345');
+// const srt=233;
+// const num=3432.9;
+// const bool=false;
+// const temp=null;
+// const id=Symbol('2354');
+// const anotherid=summbol('2345');
 
-const bignum=3423423542345234n;
+// const bignum=3423423542345234n;
 
 
 // Reference (Non primitive )
@@ -23,4 +23,29 @@ const bignum=3423423542345234n;
 // const myfunction =function(){
 //     console.log("hello Saurabh");
 // }
-console.log(typeof num);
+// console.log(typeof num);
+
+
+// ********************************* ++ *****************************
+
+// Stack (Primitive), Heap (Non -Primitive )
+
+let myYoutubename="Saurabh";
+let anothername=myYoutubename;
+anothername="chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+
+
+let userOne={
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+let userTwo=userOne;
+userTwo.email='saurabh@124';
+
+
+console.log(userOne);
+console.log(userTwo);
