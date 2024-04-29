3cbe2d691f868d92179e8d628cd53c3a2837fc65
Saurabh-kumar95 <154539057+Saurabh-kumar95@users.noreply.github.com>
2024-04-29 16:05:53 +0000

    math

---
+++ 01_basic/nums_math.js
@@ -0,0 +1,23 @@
+const score=400;
+const balance=new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const otherNumber=123.8976;
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds=100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ******************************** Math  ***************************
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.sqrt(525));
+
